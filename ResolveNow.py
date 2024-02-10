e9a55583e09d45aaab1481a9f11f2d814fd9b0a0
Edwyn romero <edwynromero3@gmail.com>
2024-02-10 12:12:00 -0500

    Add two exercises of the Resolve Now

---
+++ ResolveNow.py
@@ -0,0 +1,10 @@
+side_length = int(input("Enter the side length of the surface area: "))
+surface_area = side_length * side_length
+print(f"The surface area of the square is: {surface_area}")
+
+print("Other exercises---------------------------------------")
+number_one = int(input("Enter the firts number: "))
+number_two = int(input("Enter the second number: "))
+numbers_sum = number_one + number_two
+numbers_prod = number_one * number_two
+print(f"The sum of the numbers is: {numbers_sum} and the multiplication of the numbers is: {numbers_prod} ")
