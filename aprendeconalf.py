a2d8654643a306a94b708b43db927a70acd7432e
Edwyn romero <edwynromero3@gmail.com>
2024-02-10 11:32:09 -0500

    Add the second exercises of python

--- aprendeconalf.py
+++ aprendeconalf.py
@@ -1,2 +1,10 @@
 name = "alf"
 print(f"hi, {name}")
+
+print(type(name))
+list = [1, "dos", [3,4], True]
+print(type(list))
+dicti = {"pi":3.1416, "e":2.718}
+print(type(dicti))
+tuples = (1,"dos", 3)
+print(type(tuples))
