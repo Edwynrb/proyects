8d1378030d73518f9571797f3d82baf79cc6470d
Edwyn romero <edwynromero3@gmail.com>
2024-02-16 19:04:36 -0500

    Add try and except to code

--- ResolveNow.py
+++ ResolveNow.py
@@ -1,19 +1,25 @@
-"""
-side_length = int(input("Enter the side length of the surface area: "))
-surface_area = side_length * side_length
-print(f"The surface area of the square is: {surface_area}")
-
+try:
+    side_length = int(input("Enter the side length of the surface area: "))
+    surface_area = side_length * side_length
+    print(f"The surface area of the square is: {surface_area}")
+except ValueError:
+    print("Invalid input, Please enter a valid integer")
 print("Other exercises---------------------------------------")
-number_one = int(input("Enter the firts number: "))
-number_two = int(input("Enter the second number: "))
-numbers_sum = number_one + number_two
-numbers_prod = number_one * number_two
-print(f"The sum of the numbers is: {numbers_sum} and the multiplication of the numbers is: {numbers_prod} ")
-
+try:
+    number_one = int(input("Enter the firts number: "))
+    number_two = int(input("Enter the second number: "))
+    numbers_sum = number_one + number_two
+    numbers_prod = number_one * number_two
+    print(f"The sum of the numbers is: {numbers_sum} and the multiplication of the numbers is: {numbers_prod} ")
+except ValueError:
+    print("Invalid input, Please enter a valid integer")
 print("Other exercises---------------------------------------")
-product_quantity = int(input("Enter the products for buying: "))
-product_price = int(input("Enter the price of the product: "))
-amount_pay = product_quantity * product_price
-print(f"the total amount to pay for client is: {amount_pay}")
-
-"""
+try: 
+    product_quantity = int(input("Enter the products for buying: "))
+    product_price = int(input("Enter the price of the product: "))
+    amount_pay = product_quantity * product_price
+    print(f"the total amount to pay for client is: {amount_pay}")
+except ValueError:
+    print("Invalid input, Please enter a valid integer")
+print("Other exercises---------------------------------------")
+# side_length_two = int("Enter side length of the perimeter")
--- aprendeconalf.md
+++ aprendeconalf.md
@@ -1,2 +1,3 @@
 1. create variable name  and print message hi with variable.
-2. to show diference type of date in python(str, list, dict and tuple add int and float). 
+2. to show diference type of date in python(str, list, dict and tuple add int and float).
+3. arithmetic operators in python  Addition, Subtraction, Multiplication, Division, int division Exponentiation or Power
