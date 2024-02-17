8e44983f07d196ad8f1c232e9ce709305ecbf183
edwyn <edu88_1@hotmail.com>
2024-02-17 12:31:07 -0500

    finished exercises of average

--- ResolveNow.py
+++ ResolveNow.py
@@ -41,3 +41,14 @@ try:
     
 except ValueError:
     print("Invalid input, Please enter a value integer")
+print(" Perform a program that reads four numerical values and inform your sum and average.")   
+try:
+    num_one = int(input("Enter the first number "))
+    num_two = int(input("Enter the second number "))
+    num_three = int(input("Enter the third number "))
+    num_fourh = int(input("Enter the fourth number ")) 
+    average_nums = num_one + num_two + number_three + num_fourh / 4
+    print(f"The average of the four numbers is {average_nums}")
+
+except ValueError:
+    print("Invalid input, Please enter a value integer")
