d6daad1abe42d47620e0eccc015b4496627c9c88
edwyn <edu88_1@hotmail.com>
2024-02-17 11:56:03 -0500

    finished exercises

--- ResolveNow.py
+++ ResolveNow.py
@@ -31,10 +31,13 @@ except ValueError:
     print("Invalid input, Please enter a valid integer")    
 print(" Write a program in which four numbers are enter, calculate and inform the sum of the first two and the product of the third and the fourth.")
 try:
-    number_onee
-    number_twoo
-    number_three
-    number_fourth
+    number_onee = int(input("Enter the first number "))
+    number_twoo = int (input("Enter the second number "))
+    number_three = int (input("Enter the third number "))
+    number_fourth = int (input("Enter the fourth number "))
+    sum_first_numbers = number_onee + number_twoo
+    product_seconds_numbers = number_three * number_fourth
+    print(f"the sum of the two first number is {sum_first_numbers} and the product of two seconds numbers is {product_seconds_numbers} ")
     
 except ValueError:
     print("Invalid input, Please enter a value integer")
