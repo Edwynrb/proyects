54fd9ebcc2fd215e466ddb4637baecf59311529d
Edwyn romero <edwynromero3@gmail.com>
2024-02-19 16:54:02 -0500

    Add and resolve exercises for clowns and dolls

--- ResolveNow.py
+++ ResolveNow.py
@@ -78,3 +78,12 @@ years_numbers = int(input("Enter numbers of years "))
 capital_obtained = amount_invest * (1 + annual_interest/100)**years_numbers
 print(f"The capital obtained is {capital_obtained}")
 
+print("A toy store is very successful in two of its products: clowns and dolls. They usually sell by mail and the logistics company charges them by weight of each package, so they must calculate the weight of the clowns and dolls that will appear in each package on demand. Each clown weighs 112 g and each doll 75 g. Write a program that reads the number of clowns and dolls sold in the last order and calculates the total weight of the package that will be shipped.")
+CLOWNS_WEIGHT = 112
+DOLLS_WEIGHT = 75
+clowns_numbers = int(input("Enter numbers of clowns for package "))
+dolls_numbers = int(input("Enters numbers of dolls for package "))
+package_weight = (clowns_numbers *CLOWNS_WEIGHT) + (dolls_numbers * DOLLS_WEIGHT)
+print(f"The total weight of the package is {package_weight}") 
+
+
