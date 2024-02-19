e88c27c181dbedf21ae781ee6ac82e63d22bd475
Edwyn romero <edwynromero3@gmail.com>
2024-02-19 18:20:49 -0500

    Separate salary

---
+++ salary.py
@@ -0,0 +1,7 @@
+print("Calculate the monthly salary of an operator knowing the number of hours worked and the value per hour.")
+
+worked_hour = int(input("Enter hours worked "))
+value_hour  = int(input("Enter value per hour "))
+monthly_salary = worked_hour * value_hour
+print(f"The salary monthly of the operator is {monthly_salary} ")
+     
