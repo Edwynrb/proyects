a7d8ee165869c0e32cf310c832cae6df424ddce5
Edwyn romero <edwynromero3@gmail.com>
2024-02-19 18:30:00 -0500

    two hole numbers

---
+++ twoholenumbers.py
@@ -0,0 +1,6 @@
+print("Carry up the loads of 2 whole numbers on the keyboard and print your sum and your product")
+number_one = int(input("Enter the firts number: "))
+number_two = int(input("Enter the second number: "))
+numbers_sum = number_one + number_two
+numbers_prod = number_one * number_two
+print(f"The sum of the numbers is: {numbers_sum} and the multiplication of the numbers is: {numbers_prod} ")
