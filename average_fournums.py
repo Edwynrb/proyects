bf68d9e969fda10acd8f00844310cb327d5e07bc
Edwyn romero <edwynromero3@gmail.com>
2024-02-19 18:25:13 -0500

    Separate and fix average four numbers

---
+++ average_fournums.py
@@ -0,0 +1,8 @@
+print(" Perform a program that reads four numerical values and inform your sum and average.")   
+
+num_one = int(input("Enter the first number "))
+num_two = int(input("Enter the second number "))
+num_three = int(input("Enter the third number "))
+num_fourh = int(input("Enter the fourth number ")) 
+average_nums = num_one + num_two + num_three + num_fourh / 4
+print(f"The average of the four numbers is {average_nums}")
