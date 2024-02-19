89713ec774237315d1dd171d0cef81e5d5d22c9c
Edwyn romero <edwynromero3@gmail.com>
2024-02-19 18:28:20 -0500

    Separate surface area+

---
+++ surface_area.py
@@ -0,0 +1,4 @@
+print("Find the surface area of a square knowing the value of one side")
+side_length = int(input("Enter the side length of the surface area: "))
+surface_area = side_length * side_length
+print(f"The surface area of the square is: {surface_area}")
