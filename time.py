3ba244c04d678883d2744270a86d873bd8f26e4c
edwyn <edu88_1@hotmail.com>
2024-02-23 11:39:35 -0500

    Add or init exercises of time

---
+++ time.py
@@ -0,0 +1,11 @@
+print("Make an algorithm where an amount of seconds is entered and show the hours, minutes and seconds in tha amount. Make an algorithm to show the hours, minutes and seconds in a total number of seconds entered by keyboard.")
+
+# Make variables or constans 
+
+# Process for create app
+
+# Messages of aplication 
+
+
+
+
