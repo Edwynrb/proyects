7b7c819620f0d67834d0cff487153a0230bf88f9
Edwyn romero <edwynromero3@gmail.com>
2024-02-24 14:29:48 -0500

    Add and resolve area and perimeter of a square

---
+++ square.py
@@ -0,0 +1,11 @@
+# Statement of the problem
+print("Make a program that shows the area and perimeter of a square. ")
+
+# Create variables and input user
+side_length      = int(input("Enter side of the square: "))
+perimeter_square = side_length * 4
+square_area      = side_length * side_length
+
+# Output for screen 
+print(f"The area of the square is : {square_area} and the perimeter of the square area is: {perimeter_square} ")
+
