b3293956e5bd88cbc2f0708c9d68a5cc5cdb4cfe
Edwyn romero <edwynromero3@gmail.com>
2024-02-26 10:15:44 -0500

    Add celsius to fahrenheit

---
+++ celstofahr.py
@@ -0,0 +1,9 @@
+# Make a program to convert a temperature entered from Celsius to Fahrenheit.
+# in reverse
+
+# create variables and input for user
+c_grades = int(input("Enter the grades temperatura: "))
+# algorithms for convert celcius to fahrenheit 
+f_grades = (9/5)*c_grades+32
+# output for the screen
+print(f"The convertion for celsius {c_grades} to fahrenheit is {f_grades}")
