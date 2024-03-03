40a545e42283a279ac086144f9b942d126568e37
Edwyn romero <edwynromero3@gmail.com>
2024-03-02 20:15:55 -0500

    add statement at firts exercises

--- surface_area.py
+++ surface_area.py
@@ -11,6 +11,11 @@ block to handle the case where the input cannot be converted to a float (i.e., i
 # Positive Number Check: Added a check to ensure that the input value is greater than zero. 
 If the value is less than or equal to zero, an error message is printed, and the loop continues.
 
+# We prompt the user to enter the length of one side of the square until they type "done" to finish.
+The valid side lengths are stored in a list side_lengths.
+We calculate the surface area for each side length and store it in a dictionary surface_area_dict, where the side length is the key and the surface area is the value.
+Finally, we print the surface area for each side length stored in the dictionary.
+
 # Convert in function
 
 # Convert in oop 
