701e5a28edb2cea47c4b210ab3738d11ec66f024
Edwyn romero <edwynromero3@gmail.com>
2024-03-02 19:27:26 -0500

    add fixes

---
+++ week.py
@@ -0,0 +1,7 @@
+"""
+Make a program where a number of 7 to 7 is entered and show the day of the corresponding week,
+understanding that number 1 belongs to Monday.summary_
+"""
+# Get user input 
+# Algoritmhs for resolve
+# Display results
