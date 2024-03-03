ae16cfdf1d1ae6c11a0270b88f608d6fdf07d097
Edwyn romero <edwynromero3@gmail.com>
2024-03-02 20:10:36 -0500

    Add fixes1

--- week.py
+++ week.py
@@ -2,6 +2,8 @@
 Make a program where a number of 7 to 7 is entered and show the day of the corresponding week,
 understanding that number 1 belongs to Monday.summary_
 """
-# Get user input 
+# Get user input
+numbers_day = int(input("Enter the number for convert to day: ")) 
 # Algoritmhs for resolve
+
 # Display results
