b1db07f43ffada8f1870461549edf90ae117a4a9
Edwyn romero <edwynromero3@gmail.com>
2024-05-05 17:04:38 -0500

    convert twoholenumbers to oop

--- twoholenumbers.py
+++ twoholenumbers.py
@@ -22,30 +22,19 @@ print(f"the sum of the numbers is {sum_numbers} and the product of the numbers i
 class Whole_numbers:
     def __init__(self):
         # initialize constructor method to initialize the whole numbers object with numbers  
-        self.num1 = 0
-        self.num2 = 0
+        self.number_one = int(input("Enter the first number: "))
+        self.number_two = int(input("Enter the second number: "))
         
-
-        
-    # Creating method to enter inputs    
-    def get_inputs(self):
-        self.number_one     = int(input("Enter the first number: ")) 
-        self.number_two     = int(input("Enter the second number: ")) 
     # Method for calculate the sum of the two numbers    
     def calculate_sum(self):
-        self.sum_result = self.number_one + self.number_two
+        return self.number_one + self.number_two
     # Method for calculate the product of the two numbers
     def calculate_product(self):
-        self.product_result = self.number_one * self.number_two
-    # Method for display results 
-    def display_results(self):
-        print(f"The result of the sum is: {self.sum_result} and result of the multiplication is {self.product_result}")
-# instance objects
-def main():
-    whole_numbers = Whole_numbers()
-    whole_numbers.get_inputs()
-    whole_numbers.calculate_sum()
-    whole_numbers.calculate_product()
-    whole_numbers.display_results()
-if __name__ =="__main__":
-    main()
+        return self.number_one * self.number_two
+ # instance objects
+whole_numbers = Whole_numbers()
+sum_result=whole_numbers.calculate_sum()
+product_result=whole_numbers.calculate_product()
+print(f"The result of the sum is: {sum_result} and result of the multiplication is {product_result}")
+
+
